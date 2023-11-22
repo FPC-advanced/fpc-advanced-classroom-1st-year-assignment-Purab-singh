b3743df875f155cfdce911bae9b6c05f6c2c900b
Purab-singh <purabs.bsc23@rvu.edu.in>
2023-11-22 16:04:50 +0530

    committed 11/22/2023, 4:04:50 PM

--- set02/problem06.c
+++ set02/problem06.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 void input_string(char *a);
-void str_reverse(char *a, char *rev_str);
-void output(char *a, char *rev_str);
+void str_reverse(char *a);
+void output(char *a);
 int main()
 {
     char a[100],rev_str[100],reverse_a[100];
     input_string(a);
-    str_reverse(a,rev_str);
-    output(a,rev_str);
+    str_reverse(a);
+    output(a);
     return 0;
 }
 void input_string(char *a)
@@ -15,23 +15,26 @@ void input_string(char *a)
     printf("enter the string");
     scanf("%s",a);
 }
-void str_reverse(char *a,char *rev_str)
+void str_reverse(char *a)
 {
     int i,end,j;
     
     for(i=1;a[i]!='\0';i++);
-    end=i-1;
-    for(i=0,j=0;a[i]!='\0';j++,i++)
+    j=i*2;
+    for(i=0;a[i]!='\0';i++)
     {
-        rev_str[j]=a[end];
+        a[j]=a[i];
+        j=j-1;
         
-        end=end-1;
+        
+        
     }
-    rev_str[i]='\0';
+
+    
     
 
 }
-void output(char *a,char *rev_str)
+void output(char *a)
 {
-    printf("the reversed string is %s",rev_str);
+    printf("the reversed string is %s",a);
 }
